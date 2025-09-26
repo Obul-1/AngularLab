c36caa62c6f9ce1ea92cfc8241b62f1e82f30155
Obul-1 <229x1a0566@gprec.ac.in>
2025-09-26 14:06:37 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// import { ProfileComponent } from './profile/profile.component';
+// import { AboutComponent } from './about/about.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ProfileComponent,
+//     AboutComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [
+//     provideClientHydration(),
+//     provideAnimationsAsync()
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
